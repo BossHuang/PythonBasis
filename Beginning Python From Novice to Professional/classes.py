748ef4d7b182bb0f997baf8072f428248063ba76
BossHuang <myheromyjoe@163.com>
2017-09-14 21:19:54 +0800

    class

---
+++ Beginning Python From Novice to Professional/classes.py
@@ -0,0 +1,74 @@
+__author__ = 'leihuang'
+
+class Person():
+
+    def setName(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def greet(self):
+        print "I'm %s" % self.name
+
+man = Person()
+man.setName("L")
+man.greet()
+print man.name
+
+class PrivateDemo():
+    def __in(self):
+        print "in"
+    def out(self):
+        print "out"
+        self.__in()
+
+s = PrivateDemo()
+s.out()
+#s.__in()
+
+class MemberCount():
+    members = 0
+    def init(self):
+        MemberCount.members += 1
+
+m1 = MemberCount()
+m1.init()
+print MemberCount.members
+m2 = MemberCount()
+m2.init()
+print MemberCount.members
+m1.members = "T"
+print m1.members
+print m2.members
+
+class Father():
+    def init(self):
+        pass
+    def do(self):
+        pass
+
+class Mother():
+    def init(self):
+        pass
+    def say(self):
+        pass
+
+class Sun(Father, Mother):
+    pass
+
+print issubclass(Sun, Father)
+print issubclass(Father, Sun)
+print issubclass(Mother, Father)
+
+print Sun.__bases__
+
+s = Sun()
+print isinstance(s, Sun)
+print s.__class__
+
+sun = Sun()
+print hasattr(sun,'say')
+print hasattr(sun, 'look')
+print callable(getattr(sun,'say'))
+print hasattr(getattr(sun, 'say'), '__call__')
