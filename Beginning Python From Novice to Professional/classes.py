389ea55ed81713e4f351b822966632d99daa9d77
BossHuang <myheromyjoe@163.com>
2017-09-16 12:18:42 +0800

    property

--- Beginning Python From Novice to Professional/classes.py
+++ Beginning Python From Novice to Professional/classes.py
@@ -101,6 +101,80 @@ class B(A):
 b = B()
 b.hello()
 
+def checkIndex(key):
+    if not isinstance(key, (int, long)):
+        raise TypeError
+    if key < 0:
+        raise IndexError
 
+class ArithmeticSequence:
+    def __init__(self, start = 0, step = 2):
+        self.start = start
+        self.step = step
+        self.changed = {}
+    def __getitem__(self, key):
+        checkIndex(key)
+        try:
+            return self.changed[key]
+        except KeyError:
+            return self.start + key*self.step
+    def __setitem__(self, key, value):
+        checkIndex(key)
+        self.changed[key] = value
 
+s = ArithmeticSequence(2, 3)
+print s[1]
+
+class CounterList(list):
+    def __init__(self, *args):
+        super(CounterList, self).__init__(*args)
+        self.count = 0
+    def __getitem__(self, index):
+        self.count += 1
+        return super(CounterList, self).__getitem__(index)
+c = CounterList(range(3))
+print c[0]
+print c.count
+
+__metaclass__ = type
+class Rectangle:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+    def setSize(self, size):
+        self.width, self.height = size
+    def getSize(self):
+        return self.width, self.height
+    size = property(getSize, setSize)
+
+r = Rectangle()
+r.size = (5,6)
+print r.width, r.height
+r.width = 7
+print r.size
+
+__metaclass__ = type
+class A:
+    def sm():
+        print "static method"
+    sm = staticmethod(sm)
+    def cm(cls):
+        print "class method"
+    cm = classmethod(cm)
+
+A.cm()
+A.sm()
+
+__metaclass__ = type
+class A:
+    @staticmethod
+    def sm():
+        print "static method"
+
+    @classmethod
+    def cm(cls):
+        print "class method"
+
+A.cm()
+A.sm()
 
