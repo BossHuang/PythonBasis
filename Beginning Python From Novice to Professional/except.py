9e04d6698dc651eeeb91548c5a6db207c1f1570e
BossHuang <myheromyjoe@163.com>
2017-09-18 23:41:08 +0800

    except

--- Beginning Python From Novice to Professional/except.py
+++ Beginning Python From Novice to Professional/except.py
@@ -1,1 +1,44 @@
 __author__ = 'leihuang'
+
+import exceptions
+#print dir(exceptions)
+
+class SomeCustomException(Exception):
+    pass
+
+def division(x,y):
+    try:
+        return x/y
+    except ZeroDivisionError:
+        print "y can not be 0"
+    except TypeError:
+        print "x or y is not a number"
+
+print division(1.0, 0.0)
+
+def division(x,y):
+    try:
+        return x/y
+    except:
+        print "x or y is wrong"
+
+def division(x,y):
+    try:
+        return x/y
+    except Exception, e:
+        print e
+division(1,0)
+
+def division(x,y):
+    try:
+        print "try"
+        return x/y
+    except:
+        print "except"
+    else:
+        print "else"
+    finally:
+        print "finally"
+
+division(1,0)
+division(1,2.0)
