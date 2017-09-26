dc89c720f87ac4b4ce2ddc44e52be1a4cdf1c58f
BossHuang <myheromyjoe@163.com>
2017-09-27 00:15:38 +0800

    shelve

--- Beginning Python From Novice to Professional/modules.py
+++ Beginning Python From Novice to Professional/modules.py
@@ -1,84 +1,84 @@
 __author__ = 'leihuang'
 
-# print "---"
-# import Hi
-# print "---"
-# import Hi
-# print "---"
-# Hi = reload(Hi)
-# print "---"
-#
-# import Hi2
-# Hi2.Hello()
-#
-# from HelloWrold import Hello
-# import HelloWrold.World
-#
-# import copy
-# print [n for n in dir(copy) if not n.startswith('_')]
-# print copy.__all__
-#
-# print copy.copy.__doc__
-# help(copy.copy)
-#
-# print range.__doc__
-#
-# import copy
-# print copy.__file__
-#
-# a = set([1,2,3,4])
-# b = set([2,3,4,5])
-# print a.union(b)
-# print a | b
-#
-# print a.intersection(b)
-# print a & b
-#
-# print a.difference(b)
-# print a - b
-#
-# print a.symmetric_difference(b)
-# print a ^ b
-#
-# c = a & b
-# print c.issubset(a)
-# print c.issuperset(a)
-# print a.copy() is a
-# print a.copy() == a
-#
-# a = set([1,2])
-# b = set([2,3])
-# a.add(frozenset(b))
-# print a
-#
-# from heapq import *
-# data = [4,1,3,6]
-# heap = []
-# for num in data:
-#     heappush(heap, num)
-# print heap
-#
-# heapify(data)
-# print data
-#
-# from collections import deque
-# q = deque(range((3)))
-# q.append(5)
-# q.appendleft(6)
-# print q
-#
-# print q.pop()
-# print q.popleft()
-# print q
-#
-# q.rotate(1)
-# print q
-#
-# q.rotate(-2)
-# print q
-#
-# import time
-# print time.asctime()
+print "---"
+import Hi
+print "---"
+import Hi
+print "---"
+Hi = reload(Hi)
+print "---"
+
+import Hi2
+Hi2.Hello()
+
+from HelloWrold import Hello
+import HelloWrold.World
+
+import copy
+print [n for n in dir(copy) if not n.startswith('_')]
+print copy.__all__
+
+print copy.copy.__doc__
+help(copy.copy)
+
+print range.__doc__
+
+import copy
+print copy.__file__
+
+a = set([1,2,3,4])
+b = set([2,3,4,5])
+print a.union(b)
+print a | b
+
+print a.intersection(b)
+print a & b
+
+print a.difference(b)
+print a - b
+
+print a.symmetric_difference(b)
+print a ^ b
+
+c = a & b
+print c.issubset(a)
+print c.issuperset(a)
+print a.copy() is a
+print a.copy() == a
+
+a = set([1,2])
+b = set([2,3])
+a.add(frozenset(b))
+print a
+
+from heapq import *
+data = [4,1,3,6]
+heap = []
+for num in data:
+    heappush(heap, num)
+print heap
+
+heapify(data)
+print data
+
+from collections import deque
+q = deque(range((3)))
+q.append(5)
+q.appendleft(6)
+print q
+
+print q.pop()
+print q.popleft()
+print q
+
+q.rotate(1)
+print q
+
+q.rotate(-2)
+print q
+
+import time
+print time.asctime()
 
 import time
 import random
@@ -95,5 +95,20 @@ print a
 print random.sample(a,2)
 print a
 
+import shelve
+s = shelve.open('test.dat')
+s['x'] = ['a','b']
+print s['x']
+s['x'].append('d')
+print s['x']
+
+import shelve
+s = shelve.open('test.dat')
+s['x'] = ['a','b']
+print s['x']
+temp = s['x']
+temp.append('d')
+s['x'] = temp
+print s['x']
 
 
