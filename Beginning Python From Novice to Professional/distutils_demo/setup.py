2db7f9015bd731d67e36363c01d65d7326b70325
BossHuang <myheromyjoe@163.com>
2018-03-04 23:40:19 +0800

    distutils

---
+++ Beginning Python From Novice to Professional/distutils_demo/setup.py
@@ -0,0 +1,9 @@
+__author__ = 'leihuang'
+
+from distutils.core import setup
+
+setup(name='hello',
+      version='0.1',
+      description='a simple demo',
+      author='lei',
+      py_modules=['hello'])
