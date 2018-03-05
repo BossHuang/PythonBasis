890ab22820d6d8d165d3e02dabd70589905d88ad
BossHuang <myheromyjoe@163.com>
2018-03-05 22:05:12 +0800

    compileExtension

---
+++ Beginning Python From Novice to Professional/distutils_compileExtension/setup.py
@@ -0,0 +1,9 @@
+__author__ = 'leihuang'
+
+from distutils.core import setup, Extension
+
+setup(name='palindrome',
+      version='1.0',
+      ext_modules=[
+          Extension('palindrome',
+                    ['palindrome.c','palindrome.i'])])
