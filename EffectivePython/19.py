0a786ac48f35618bd895737d20cc3ad80a6f3c98
BossHuang <myheromyjoe@163.com>
2018-04-03 22:04:10 +0800

    用关键字参数来表达可选的行为

---
+++ EffectivePython/19.py
@@ -0,0 +1,12 @@
+__author__ = 'leihuang'
+
+def remainder(number, divisor):
+    return number%divisor
+
+remainder(20,7)
+remainder(20, divisor=7)
+remainder(number=20, divisor=7)
+remainder(divisor=7, number=20)
+
+remainder(number=20, 7)
+remainder(20,number=20)
