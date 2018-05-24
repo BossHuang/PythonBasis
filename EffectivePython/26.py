97d24f7580cf7033adeea31928d6ba4ef3592e81
BossHuang <myheromyjoe@163.com>
2018-05-24 10:27:30 +0800

    只在使用Mix-in组件制作工具类时进行多重继承

--- EffectivePython/26.py
+++ EffectivePython/26.py
@@ -30,3 +30,41 @@ class BinaryTree(ToDictMixIn):
 
 tree = BinaryTree(10, left=BinaryTree(7, right=BinaryTree(9)), right=BinaryTree(13, left=BinaryTree(11)))
 print tree.to_dict()
+print tree.__dict__
+print BinaryTree.__dict__
+
+class BinaryTreeWithParent(BinaryTree):
+    def __init__(self, value, left=None, right=None, parent=None):
+        super(BinaryTreeWithParent, self).__init__(value, right=right, left=left)
+        self.parent = parent
+    def _traverse(self, key, value):
+        if isinstance(value, BinaryTreeWithParent) and key == 'parent':
+            return value.value
+        else:
+            return super(BinaryTreeWithParent, self)._traverse(key, value)
+
+root = BinaryTreeWithParent(10)
+root.left = BinaryTreeWithParent(7, parent=root)
+root.right = BinaryTreeWithParent(9, parent=root)
+print root.to_dict()
+
+import json
+class JsonMixIn(object):
+    @classmethod
+    def from_json(cls, data):
+        kwargs = json.load(data)
+        return cls(**kwargs)
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+class DatacenterRack(ToDictMixIn, JsonMixIn):
+    def __init__(self, switch=None, machines=None):
+        self.switch = Switch(**switch)
+        self.machines = [Machine(**kwargs) for kwargs in machines]
+
+class Switch(ToDictMixIn, JsonMixIn):
+    pass
+
+class Machine(ToDictMixIn, JsonMixIn):
+    pass
