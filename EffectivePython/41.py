d684e2cf30cac9467281030a5df3d90071341ae6
BossHuang <myheromyjoe@163.com>
2018-08-07 20:27:28 +0800

    考虑用concurrent.futures来实现真正的平行计算

---
+++ EffectivePython/41.py
@@ -0,0 +1,33 @@
+__author__ = 'leihuang'
+
+# Python3
+from time import time
+def gcd(pair):
+    a,b = pair
+    low = min(a,b)
+    for i in range(low,0,-1):
+        if a%i == 0 and b%i == 0:
+            return i
+
+numbers = [(1963309,2265973),(2030677,3814172),
+           (1551645,2229620),(2039045,2020802)]
+
+start = time()
+results = list(map(gcd, numbers))
+end = time()
+print("took %.3f seconds" %(end-start))
+
+from concurrent.futures import ThreadPoolExecutor
+start = time()
+pool = ThreadPoolExecutor(max_workers=2)
+results = list(pool.map(gcd, numbers))
+end = time()
+print("took %.3f seconds" %(end-start))
+
+from concurrent.futures import ProcessPoolExecutor
+start = time()
+pool = ProcessPoolExecutor(max_workers=2)
+results = list(pool.map(gcd, numbers))
+end = time()
+print("took %.3f seconds" %(end-start))
+
