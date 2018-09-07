9044cba103e6121aba268ecceb378a4b2217815b
BossHuang <myheromyjoe@163.com>
2018-09-07 22:59:51 +0800

    用包来安排模块，并提供稳固的API

---
+++ EffectivePython/50.py
@@ -0,0 +1,7 @@
+__author__ = 'leihuang'
+
+form analysis.utils import log_base2_bucket
+from frontend.utils import stringify
+
+form analysis.utils import inspect as analysis_inspect
+from frontend.utils import inspect as frontend_inspect
